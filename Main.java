0d5aa5795b151edd5e3b93f0a2c1df88e6bcd643
Josephrosenberry <31011380+Josephrosenberry@users.noreply.github.com>
2017-09-12 09:52:24 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,25 +5,46 @@ public class Main{
     Scanner scan = new Scanner (System.in);
     System.out.print ("Enter your grade: (Q to quit): ");
      grade = scan.nextLine();
+     int numGrades = 0;
+     int numA = 0, numB = 0, numC = 0, numD = 0, numF = 0;
     grade = grade.toUpperCase();
     
     
     while (!grade.equals("Q")){
-     if (grade.equals("A"))
-       System.out.print("Excellent!");
-      else if (grade.equals("B"))
-        System.out.print("Good!");
-      else if (grade.equals("C")||grade.equals("D"))
-        System.out.print("Poor");
-      else if (grade.equals("F"))
-        System.out.print("Failure");
-      else
-        System.out.print("Invalid Grade Entered");
+     if (grade.equals("A")){
+       System.out.println("Excellent!");
+       numA++;
+     }
+       
+      else if (grade.equals("B")) {
+        System.out.println("Good!");
+        numB++;
+      }
         
-      System.out.print ("Enter your grade: (Q to quit): ");
+      else if (grade.equals("C")){
+        System.out.println("Poor");
+        numC++;
+      }
+        
+      else if (grade.equals("D")){
+        System.out.println("Poor");
+        numD++;
+        }
+        
+      else if (grade.equals("F")){
+        System.out.println("Failure");
+        numF++;
+      }
+        
+      else
+        System.out.println("Invalid Grade Entered");
+        
+      System.out.println ("Enter your grade: (Q to quit): ");
       grade = scan.next();
       grade = grade.toUpperCase();
     }  
-      System.out.println("Goodbye!");
+      double gpa = (numA*4.0 + numB*3.0 + numC*2.0 + numD*1.0)/(numA + numB + numC + numD + numF);
+      System.out.printf("\n\nGPA: %.2f", gpa);
+      System.out.println(" Goodbye!");
   }
 }
