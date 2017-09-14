8b77b1c895d9cf3c49b665618b4eaeefe4c03a84
Josephrosenberry <31011380+Josephrosenberry@users.noreply.github.com>
2017-09-14 10:18:12 -0700

    Add files via upload

---
+++ PizzaOrder.java
@@ -0,0 +1,7 @@
+public class PizzaOrder
+{
+    public static void main (String [] args)
+    {
+        
+    }
+}
--- Rock.java
+++ Rock.java
@@ -14,19 +14,20 @@ public class Rock
         int computerInt;
 
         Scanner scan = new Scanner (System.in);
-        System.out.println ("Lets play Rock Paper Scissors");
+        System.out.println ("\fLets play Rock Paper Scissors");
         System.out.println ("Type R for Rock, P for Paper, S for Scissors");
+        personPlay = scan.nextLine();
         personPlay = personPlay.toUpperCase();
-        computerInt = ((Math.random()* 3));
+        computerInt = ( (int)(Math.random()* 3));
 
-        if (computerInt == 1)
+        if (computerInt == 0)
             computerPlay = "R";
-        else if (computerInt == 2)
+        else if (computerInt == 1)
             computerPlay = "P";
-        else if (computerInt == 3)
+        else if (computerInt == 2)
             computerPlay = "S";
         
-        System.out.println ("Computers choice: " + computerInt);    
+        System.out.println ("Computers choice: " + computerPlay);    
         
         if (personPlay.equals("R"))
         {
@@ -38,8 +39,32 @@ public class Rock
         else if (personPlay.equals("S")){
             System.out.println ("Your choice: Scissors");
     }
-            else
+            else {
             System.out.println ("Invalid letter entered");
-        
+        }
+       if (computerPlay.equals(personPlay))
+        System.out.println("Its a tie!");
+       else if (computerPlay.equals("S")){ 
+        if (personPlay.equals("P"))
+       System.out.println("Scissor cuts paper. You lose!!");
+        else if (personPlay.equals("R"))
+            System.out.println("Rock beats scissors. You win!");
     }
-}    
+    else if (computerPlay.equals("R")){
+        if (personPlay.equals("P"))
+            System.out.println("Paper eats rock. You win!!"); 
+        else if (personPlay.equals("S")){
+            System.out.println("Rock beats paper. You lose!");
+        }
+    else if (computerPlay.equals("S")) {
+         if (personPlay.equals("P")) 
+         System.out.println("Scissor cuts paper. You lose!"); 
+         else if (personPlay.equals("R"))
+         System.out.println("Rock crushes scisssors. You win!");
+        }
+        
+    else 
+         System.out.println("Invalid user input."); 
+    }
+}  
+}
